4c0fa206c841d192187da954e9467308b0f2e617
andriAntt <andriy.ant.work@gmail.com>
2025-01-31 11:47:52 +0100

    delete build and unused assests

--- build/static/js/404.f9a30479.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunklanding=self.webpackChunklanding||[]).push([[404],{404:(a,n,s)=>{s.r(n),s.d(n,{default:()=>u});var e=s(272),l=s(579);const u=()=>(0,l.jsx)(e.a3,{})}}]);
-//# sourceMappingURL=404.f9a30479.chunk.js.map
