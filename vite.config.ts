eaf2db852815c53ec276a7fafdc581a697614ab5
dmitry kulchitskiy <kulchitskiy.dima@gmail.com>
2025-02-03 22:13:56 +0300

    compression

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import react from "@vitejs/plugin-react";
 import * as path from "path";
 import { defineConfig } from "vite";
+import viteCompression from 'vite-plugin-compression';
 import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
@@ -15,6 +16,7 @@ export default defineConfig({
       },
       include: "**/*.svg",
     }),
+    viteCompression(),
   ],
   resolve: {
     alias: {
